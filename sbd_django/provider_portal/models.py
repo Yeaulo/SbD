928630f272f3f91774c48ac3d9aeee689b8e9a08
BeneGeb <92448111+BeneGeb@users.noreply.github.com>
2023-11-03 16:28:24 +0100

    ORM English version and migration

--- sbd_django/provider_portal/models.py
+++ sbd_django/provider_portal/models.py
@@ -3,35 +3,33 @@ from django.db import models
 # Create your models here.
 
 
-class Kunde(models.Model):
-    kundennummer_id = models.IntegerField(max_length=10, primary_key=True)
-    name = models.CharField(max_length=200)
-    vorname = models.CharField(max_length=200)
-    strasse = models.CharField(max_length=200)
-    hausnummer = models.IntegerField()
-    plz = models.IntegerField()
+class Customer(models.Model):
+    customer_id = models.IntegerField(max_length=10, primary_key=True)
+    last_name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    addresss = models.CharField(max_length=200)
+    house_number = models.IntegerField()
+    post_code = models.IntegerField()
 
-
-class Vertaege(models.Model):
-    vertragsnummer_id = models.IntegerField(max_length=10, primary_key=True)
-    preis_pro_monat = models.IntegerField()
-    bezeichnung = models.CharField(max_length=200)
-    Mindestlaufzeit = models.IntegerField(default=12)
-    Kündigungsfrist = models.IntegerField(default=3)
-
+class Contracts(models.Model):
+    contract_id = models.IntegerField(max_length=10, primary_key=True)
+    price_per_month = models.IntegerField()
+    description = models.CharField(max_length=200)
+    minimum_term = models.IntegerField(default=12)
+    notice_period = models.IntegerField(default=3)
 
 class Smartmeter(models.Model):
     smartmeter_id = models.IntegerField(max_length=10, primary_key=True)
-    kundennummer_id = models.ForeignKey(Kunde, on_delete=models.CASCADE)
-    vertragsnummer_id = models.ForeignKey(Vertaege, on_delete=models.CASCADE)
-    vertragsstart = models.DateField()
-    straße = models.CharField(max_length=200)
-    hausnummer = models.IntegerField()
-    plz = models.IntegerField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    contract = models.ForeignKey(Contracts, on_delete=models.CASCADE)
+    contract_start = models.DateField()
+    address = models.CharField(max_length=200)
+    house_numnber = models.IntegerField()
+    post_code = models.IntegerField()
 
 
-class Messdaten(models.Model):
-    messdaten_id = models.IntegerField(max_length=10, primary_key=True)
+class Measurements(models.Model):
+    measurement_id = models.IntegerField(max_length=10, primary_key=True)
     smartmeter_id = models.ForeignKey(Smartmeter, on_delete=models.CASCADE)
-    messwert = models.IntegerField()
-    messdatum = models.DateField()
+    value = models.IntegerField()
+    timestamp = models.DateField()
