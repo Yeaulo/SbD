8631cefa2d029ceeae7408fd941d189e28ecb7cd
BeneGeb <92448111+BeneGeb@users.noreply.github.com>
2023-11-03 17:54:38 +0100

    Testdaten und Veränderung im Model

--- sbd_django/provider_portal/admin.py
+++ sbd_django/provider_portal/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from provider_portal.models import Customers, Contracts, Smartmeter, Measurements
 # Register your models here.
+admin.site.register(Customers)
+admin.site.register(Contracts)
+admin.site.register(Smartmeter)
+admin.site.register(Measurements)
