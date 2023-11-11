94afaf2b22ab1c5b81dd685ecfe3647bbccf3167
BeneGeb <92448111+BeneGeb@users.noreply.github.com>
2023-11-11 09:52:19 +0100

    Creatre state and Input

--- sbd_react/src/components/UserData/InputUserData.jsx
+++ sbd_react/src/components/UserData/InputUserData.jsx
@@ -1,8 +1,8 @@
-export default function InputUserData({propertyName, value}){
+export default function InputUserData({propertyName, value, onChange}){
     return (
         <div>
             {propertyName}
-            <input type="text" value={value} readOnly/>
+            <input type="text" value={value} onChange={onChange}/>
         </div>
     )
 }
--- sbd_react/src/components/UserData/UserData.jsx
+++ sbd_react/src/components/UserData/UserData.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react"
 import InputUserData from "./InputUserData"
 
 export default function UserData(){
+    const {userData, setUserData} = useState({
+        first_name: "Bene",
+    });
+
     return (
         <div className="outer-user-data"> 
             Nutzerdaten
             <div className="inner-user-data">
-                <InputUserData propertyName="Vorname" value="Max" />
-                <InputUserData propertyName="Nachname" value="Mustermann" />
-                <InputUserData propertyName="E-Mail" value=""/>
+                <InputUserData propertyName="firstname" value={userData.first_name}/>
             </div>
         </div>
     )
 }
+{/* <InputUserData propertyName="lastname" value="Mustermann" onChange={() => handleChange("last_name")}/>
+<InputUserData propertyName="e-mail" value="" onChange={() => handleChange("e-mail")}/> */}
+    //     const handleChange = (e) => {
+    //     const { name, value } = e.target;
+    //     setCredentials(prevState => ({
+    //       ...prevState,
+    //       [name]: value
+    //     }));
+    //   };
+        // function handleChange(e, propertyName){
+        //     const value = e.target.value
+        //     setUserData(prevState => ({
+        //         ...prevState,
+        //         [propertyName]: value
+        //     }));
+        // }
