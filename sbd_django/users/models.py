7e4aaa7d740080f769f28cb54f2b9582eaeb07ea
BeneGeb <92448111+BeneGeb@users.noreply.github.com>
2023-12-05 12:20:58 +0100

    Update models.py

--- sbd_django/users/models.py
+++ sbd_django/users/models.py
@@ -8,6 +8,5 @@ class User(AbstractUser):
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
 
-
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
