3501c246033b68417e7a3009f13de6f4a92cd376
BeneGeb <92448111+BeneGeb@users.noreply.github.com>
2023-12-05 12:18:33 +0100

    Update serializers.py

--- sbd_django/users/serializers.py
+++ sbd_django/users/serializers.py
@@ -4,14 +4,16 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'name', 'email', 'password']
+        fields = ['id', 'name', 'email', 'password', 'first_name']
         extra_kwargs = {
             'password': {'write_only': True}
         }
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
+        email = validated_data.get('email', None)
+
+        instance = self.Meta.model(username = email,**validated_data)
         if password is not None:
             instance.set_password(password)
         instance.save()
